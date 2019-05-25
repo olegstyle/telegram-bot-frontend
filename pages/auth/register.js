a2d4b2d66d65f72ce8aa2f21114ef321a3dd55af
Oleh Borysenko <oleg.borisenko@morefromit.com>
2019-05-25 15:07:46 +0200

    register fix

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -14,8 +14,10 @@ class Register extends React.Component {
             name: '',
             email: '',
             password: '',
+            agree_term: false,
             fails: {},
         };
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     static async getInitialProps(context) {
@@ -27,7 +29,11 @@ class Register extends React.Component {
         if (this.state.loading) {
             return;
         }
-        this.state.fails = {};
+        if (!this.state.agree_term) {
+            this.setState({fails: {agree_term: 'fail'}});
+            return;
+        }
+        this.setState({fails: {}});
         const self = this;
         self.setState({loading: true});
         this.props.register(
@@ -42,7 +48,16 @@ class Register extends React.Component {
         );
     }
 
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({[name]: value});
+    }
+
     render() {
+        console.log(this.state.fails);
         return <AuthLayout title="Register">
             <section className="signup">
                 <div className="container">
@@ -50,43 +65,52 @@ class Register extends React.Component {
                         <div className="signup-form">
                             <h2 className="form-title">Sign up</h2>
                             <form method="POST" className="register-form" id="register-form" onSubmit={this.handleSubmit.bind(this)}>
-                                <div className="form-group">
+                                <div className={'form-group ' + (this.state.fails.hasOwnProperty('name') ? 'form-error' : '')}>
                                     <label htmlFor="name"><i className="zmdi zmdi-account material-icons-name"/></label>
                                     <input
                                         type="text"
                                         name="name"
                                         id="name"
                                         placeholder="Your Name"
-                                        onChange={e => this.setState({ name: e.target.value })}
-                                    />
+                                        onChange={this.handleInputChange} />
+                                    <div className="invalid-feedback d-block">{this.state.fails.name}&nbsp;</div>
                                 </div>
-                                <div className="form-group">
+                                <div className={'form-group ' + (this.state.fails.hasOwnProperty('email') ? 'form-error' : '')}>
                                     <label htmlFor="email"><i className="zmdi zmdi-email"/></label>
                                     <input
                                         type="email"
                                         name="email"
                                         id="email"
                                         placeholder="Your Email"
-                                        onChange={e => this.setState({ email: e.target.value })} />
+                                        onChange={this.handleInputChange} />
+                                    <div className="invalid-feedback d-block">{this.state.fails.email}&nbsp;</div>
                                 </div>
-                                <div className="form-group">
+                                <div className={'form-group ' + (this.state.fails.hasOwnProperty('password') ? 'form-error' : '')}>
                                     <label htmlFor="pass"><i className="zmdi zmdi-lock"/></label>
                                     <input
                                         type="password"
-                                        name="pass"
-                                        id="pass"
+                                        name="password"
+                                        id="password"
                                         placeholder="Password"
-                                        onChange={e => this.setState({ password: e.target.value })}
-                                    />
+                                        onChange={this.handleInputChange} />
+                                    <div className="invalid-feedback d-block">{this.state.fails.password}&nbsp;</div>
                                 </div>
                                 <div className="form-group">  </div>
-                                <div className="form-group">
-                                    <input type="checkbox" name="agree-term" id="agree-term" className="agree-term" />
-                                    <label htmlFor="agree-term" className="label-agree-term">
+                                <div className={'form-group ' + (this.state.fails.hasOwnProperty('agree_term') ? 'form-error' : '')}>
+                                    <input type="checkbox"
+                                           name="agree_term"
+                                           id="agree_term"
+                                           className="agree_term"
+                                           checked={this.state.agree_term}
+                                           onChange={this.handleInputChange} />
+                                    <label htmlFor="agree_term" className="label-agree-term">
                                         <span><span/></span>
                                         I agree all statements in &nbsp;
                                         <a href="#" className="term-service">Terms of service</a>
                                     </label>
+                                    <div className={'invalid-feedback d-block ' + (this.state.fails.hasOwnProperty('agree_term') ? 'visible' : 'invisible')}>
+                                        This field required!
+                                    </div>
                                 </div>
                                 <div className="form-group form-button">
                                     <input type="submit" name="signup" id="signup" className="form-submit"
