3427d5603c7cbc7e9083d4076cc4d35cec83ca27
olegstyle <olegstyle1@gmail.com>
2019-06-25 21:34:05 +0200

    Schedule page (#11)
    * schedules list
    
    * create and edit a schedule
    
    * show correct plan on schedule list
    
    * better style for cron builder
    
    * fix scroll

--- components/layouts/dashboard.js
+++ components/layouts/dashboard.js
@@ -6,6 +6,10 @@ import Topbar from '../nav/topbar';
 import Footer from '../nav/footer';
 import Popper from 'popper.js';
 import ConfirmStyles from 'react-confirm-alert/src/react-confirm-alert.css';
+import CronBuilderStyles from 'react-cron-builder/dist/bundle.css';
+import PerfectScrollbar from 'perfect-scrollbar';
+import PerfectScrollbarStyle from 'perfect-scrollbar/css/perfect-scrollbar.css';
+import * as $ from "jquery";
 
 export default class extends React.Component {
     static async getInitialProps(context) {
@@ -14,12 +18,23 @@ export default class extends React.Component {
         }
     }
 
+    componentDidUpdate() {
+        const scrollables = $('.scrollable, #__next');
+        if (scrollables.length > 0) {
+            scrollables.each((index, el) => {
+                new PerfectScrollbar(el);
+            });
+        }
+    }
+
     render() {
         return <div>
             <Head>
                 <title>{this.props.title}</title>
-                <style dangerouslySetInnerHTML={{__html: Styles}}/>
                 <style dangerouslySetInnerHTML={{__html: ConfirmStyles}}/>
+                <style dangerouslySetInnerHTML={{__html: CronBuilderStyles}}/>
+                <style dangerouslySetInnerHTML={{__html: PerfectScrollbarStyle}}/>
+                <style dangerouslySetInnerHTML={{__html: Styles}}/>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
                       integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
                       crossOrigin="anonymous"/>
@@ -28,8 +43,10 @@ export default class extends React.Component {
                 <Sidebar/>
                 <div className="page-container">
                     <Topbar/>
-                    <main id="mainContent" className="main-content bgc-grey-100 h-100">
-                        {this.props.children}
+                    <main className="main-content bgc-grey-100 h-100">
+                        <div id="mainContent">
+                            {this.props.children}
+                        </div>
                     </main>
                     <Footer/>
                 </div>
--- pages/schedules/index.js
+++ pages/schedules/index.js
@@ -1,17 +1,120 @@
 import React from "react";
+import Link from 'next/link';
 import DashboardLayout from '../../components/layouts/dashboard';
 import AuthService from "../../src/services/AuthService";
 import { connect } from 'react-redux';
 import actions from '../../redux/actions/auth';
+import { Table, Form, Input } from 'reactstrap';
+import api from '../../src/api';
+import { confirmAlert } from 'react-confirm-alert';
+import { toast } from 'react-toastify';
+import cronstrue from 'cronstrue';
 
-class SchedulerPage extends React.Component {
+class SchedulesPage extends React.Component {
     static async getInitialProps(context) {
         AuthService.initialShouldAuthorized(context);
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            schedules: null,
+            loading: false,
+        };
+    }
+
+    loadSchedules() {
+        if (this.state.loading) {
+            return;
+        }
+
+        this.setState({loading: true});
+        const self = this;
+        api.schedules.list().then(response => {
+            self.setState({schedules: response.data.data, loading: false});
+        }).catch(reason => {
+            console.error(reason);
+            self.setState({loading: false});
+        });
+    }
+
+    deleteSchedule(schedule, e) {
+        const self = this;
+        confirmAlert({
+            title: 'Confirm delete.',
+            message: `Are you sure that you want to delete schedule with name ${schedule.title}?`,
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => {
+                        api.schedules.delete(schedule.id).then(value => {
+                            self.loadSchedules();
+                            toast('🦄 Successfully Deleted!');
+                        }).catch(reason => {
+                            toast.warn('🦄 Delete failed!');
+                            console.error(reason);
+                            self.loadSchedules();
+                        });
+                    }
+                },
+                {
+                    label: 'No',
+                }
+            ]
+        });
+    }
+
+    componentDidMount() {
+        this.loadSchedules();
+    }
+
     render() {
-        return <DashboardLayout title="Scheduler">
-            Scheduler...
+        const scheduleRows = [];
+        for (const schedule of (this.state.schedules || [])) {
+            scheduleRows.push(
+                <tr key={schedule.id}>
+                    <td>{schedule.title}</td>
+                    <td>{cronstrue.toString(schedule.expression)}</td>
+                    <td>
+                        <Link href={{ pathname: '/schedules/edit', query: { scheduleId: schedule.id }}}>
+                            <button className="btn btn-sm btn-success m-1">
+                                Edit
+                            </button>
+                        </Link>
+                        <button className="btn btn-sm btn-danger m-1" onClick={this.deleteSchedule.bind(this, schedule)}>
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            );
+        }
+        if (scheduleRows.length === 0) {
+            scheduleRows.push(
+                <tr key="1">
+                    <td colSpan="3">Nothing to show...</td>
+                </tr>
+            );
+        }
+
+        return <DashboardLayout title="Schedules">
+            <div className="d-flex flex-row align-items-center mb-3">
+                <h5 className="mb-0 mr-3">Schedules</h5>
+                <Link href="/schedules/edit">
+                    <a href="/schedules/edit" className="btn btn-sm btn-success">Add schedule</a>
+                </Link>
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th width="300px">Plan</th>
+                    <th width="200px">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {scheduleRows}
+                </tbody>
+            </Table>
         </DashboardLayout>
     }
 }
@@ -19,4 +122,4 @@ class SchedulerPage extends React.Component {
 export default connect(
     state => state,
     actions
-)(SchedulerPage);
+)(SchedulesPage);
